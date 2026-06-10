28998a2b0414f6c993931d5da8976fccf2e88af1
Florian <florian.reischl09@htl-salzburg.ac.at>
2026-06-10 15:12:58 +0200

    refactor: improve binary search logic close #7

--- Assets/MyGame/Scripts/Calculate.cs
+++ Assets/MyGame/Scripts/Calculate.cs
@@ -1,30 +1,33 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
 public class Calculate : MonoBehaviour
 {
+    // Variablen wurden angegeben 
     [SerializeField] private int min;
     [SerializeField] private int max;
-
     private int guess;
-
     [SerializeField] private TextMeshProUGUI guessText;
-
     public Button higherButton;
     public Button lowerButton;
     public Button correctButton;
 
-    void Start()
+    private void Start()
     {
+        // Funktion wird direkt beim Start ausgeführt
         CalculateFirstGuess();
     }
-    void CalculateFirstGuess()
+    private void CalculateFirstGuess()
     {
+        //Guesses werden bearbeitet
         guess = (min + max) / 2;
         UpdateUI();
     }
-    void UpdateUI()
+    private void UpdateUI()
     {
+        // Überarbeitet die Guessvariable
         if (guessText != null)
         {
             guessText.text = "Guess: " + guess;
@@ -32,20 +35,29 @@ public class Calculate : MonoBehaviour
     }
     public void OnHigherPressed()
     {
+        // Funktion für den Higher Button
         min = guess + 1;
         CalculateFirstGuess();
     }
     public void OnLowerPressed()
     {
+        // Funktion für den Lower Button
         max = guess - 1;
         CalculateFirstGuess();
     }
     public void OnCorrectPressed()
     {
+        // Funktion für den Correct Button
         guessText.text = "I guessed your Numberrrrr!";
-
         higherButton.interactable = false;
         lowerButton.interactable = false;
         correctButton.interactable = false;
+    }
+    private IEnumerator TimerForRestart()
+    {
+        // Restartet das Game nach bestimmter Zeit
+        yield return new WaitForSeconds(5);
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        SceneManager.LoadScene(0);
     }
 }
