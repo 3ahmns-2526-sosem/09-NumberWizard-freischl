4627ec60fa9c48a0c7488c39f12feddcd3434259
Florian <florian.reischl09@htl-salzburg.ac.at>
2026-06-10 14:05:58 +0200

    Issue3: calculate first guess

---
+++ Assets/MyGame/Scripts/Calculate.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+
+public class Calculate : MonoBehaviour
+{
+    [SerializeField] private int min;
+    [SerializeField] private int max;
+
+    private int guess;
+
+    [SerializeField] private TextMeshProUGUI guessText;
+
+    void Start()
+    {
+        CalculateFirstGuess();
+    }
+
+    void CalculateFirstGuess()
+    {
+        guess = (min + max) / 2;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        if (guessText != null)
+        {
+            guessText.text = "Guess: " + guess;
+        }
+    }
+}
