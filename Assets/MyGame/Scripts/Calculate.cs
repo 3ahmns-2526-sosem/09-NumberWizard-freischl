e1ab46d9a26ce6a453e2dc2a31ed23a628cf4e85
Florian <florian.reischl09@htl-salzburg.ac.at>
2026-06-10 14:39:39 +0200

    feat: add lower button logic close#5

--- Assets/MyGame/Scripts/Calculate.cs
+++ Assets/MyGame/Scripts/Calculate.cs
@@ -9,18 +9,15 @@ public class Calculate : MonoBehaviour
     private int guess;
 
     [SerializeField] private TextMeshProUGUI guessText;
-
     void Start()
     {
         CalculateFirstGuess();
     }
-
     void CalculateFirstGuess()
     {
         guess = (min + max) / 2;
         UpdateUI();
     }
-
     void UpdateUI()
     {
         if (guessText != null)
@@ -31,6 +28,11 @@ public class Calculate : MonoBehaviour
     public void OnHigherPressed()
     {
         min = guess + 1;
+        CalculateFirstGuess();
+    }
+    public void OnLowerPressed()
+    {
+        max = guess - 1;
         CalculateFirstGuess();
     }
 }
